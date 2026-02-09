21d1cf2365dab20b0abdaf30b737f3df25e1b97f
hebcodee <herberth.ferreira6@gmail.com>
2026-02-09 17:53:21 -0300

    feat: Add UserRepository

---
+++ src/main/java/dev/heb/springmongodb/repositores/UserRepository.java
@@ -0,0 +1,10 @@
+package dev.heb.springmongodb.repositores;
+import dev.heb.springmongodb.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
